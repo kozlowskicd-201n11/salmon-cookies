736b3885130a2404e0c1fbd9bc441715f2c2f285
Chris Kozlowski <kozlowskicd@gmail.com>
2018-05-26 09:42:55 -0700

    working on constructor

--- app.js
+++ app.js
@@ -1,78 +1,16 @@
 'use strict';
 
-var randCustomers = function(min, max) {
-    return (Math.random() * (max-min) + min)
+var locations = [];
+
+function MakeLocation(location, min, max, avg) {
+    this.storeName = location;
+    this.minCustPerHour = min;
+    this.maxCustPerHour = max;
+    this.avgNumSoldPerCust = avg;
+    this.storeHours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
+    this.randCustomers = [];
+    this.cookiesPerHour = [];
+    this.totalCookiesSold = 0;
+    locations += locations.push(this);
 }
 
-var storeHours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
-//document.getElementById('hours').textContent = storeHours;
-
-var makeSales = function(store) {
-    for (var i = 0; i < 15; i++) {
-        store.cookiesSold.push(Math.round(randCustomers(store.min, store.max) * store.avg));       
-    }
-}
-var firstPike = {
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    cookiesSold: [],
-}
-
-
-makeSales(firstPike);
-//document.getElementById('firstPike').textContent = firstPike.cookiesSold;
-
-
- var makePerHour = function(store) {
-        for (var i = 0; i < 15; i++) {
-        var tableId = document.getElementById(store);
-        var newRow = tableId.insertRow(-1);
-        var newCell = newRow.insertCell(0);
-        var newText = document.createTextNode(storeHours[i] + ' --- ' + firstPike.cookiesSold[i] + ' cookies sold');
-        newCell.appendChild(newText);
-    }
-}
-
-
-makePerHour('firstPike');
-
-// var seaTac = {
-//     min: 3,
-//     max: 24,
-//     avg: 1.2,
-//     cookiesSold: [],
-// }
-
-// makeSales(seaTac);
-// document.getElementById('seaTac').textContent = seaTac.cookiesSold;
-
-// var seaCenter = {
-//     min: 11,
-//     max: 38,
-//     avg: 3.7,
-//     cookiesSold: [],
-// }
-
-// makeSales(seaCenter);
-// document.getElementById('seaCenter').textContent = seaCenter.cookiesSold;
-
-// var capHill = {
-//     min: 20,
-//     max: 38,
-//     avg: 2.3,
-//     cookiesSold: [],
-// }
-
-// makeSales(capHill);
-// document.getElementById('capHill').textContent = capHill.cookiesSold;
-
-// var alki = {
-//     min: 2,
-//     max: 16,
-//     avg: 4.6,
-//     cookiesSold: [],
-// }
-
-// makeSales(alki);
-// document.getElementById('alki').textContent = alki.cookiesSold;
